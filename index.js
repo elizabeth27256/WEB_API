8f9c60addc0955ba4926336dbbd10959f8b3777d
Estudiante Ltic <estudiante@sistemas.local>
2025-06-18 23:01:37 -0500

    Commit 2

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
+require('dotenv').config();
 let express = require('express');
-let app = express();
 let tareasRoutes = require('./routes/tareas');
+let mongoose = require('mongoose');
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("Conectado a MongoDB Atlas"))
+    .catch((err) => console.error("Error", err));
+
+// console.log(process.env.MONGO_URI) //Esto sirve para poner nuestras credenciales 
+
+let app = express();
 
 app.use(express.json())
-app.use('/tareas',tareasRoutes);
+app.use('/tareas', tareasRoutes);
 
-app.listen(3000, () =>{
+app.listen(3000, () => {
     console.log("Servidor corriendo en el puerto 3000")
 })
+.listen
+
